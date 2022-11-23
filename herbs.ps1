d3506a5206055920f9138290ab3e705863ad899e
Kevin Burkeland <kevin@burke.land>
2022-11-23 09:07:32 -0800

    Update herbs.ps1

--- herbs.ps1
+++ herbs.ps1
@@ -1,11 +1,17 @@
+#imports dice class
 using module ./modules/dice.psm1
 $dice = [Dice]::new()
+#imports herb data
 $herbs = Import-Clixml -Path .\Data\herbs.xml
+#creates gathered array
 $gathered = @()
 
+#writes out user info
 $rolls = Read-Host "how many d100s to roll?"
 $biome = Read-Host "what biome? (arctic, caves, river, forest)"
+#rolls d100s
 $results=$dice.GetD100($rolls)
+#sorts results
 $results|ForEach-Object {
     if ($_ -in 1..39) {
         $gathered += $herbs | Where-Object Biome -like $biome | Where-Object Rarity -like "common" | Get-Random
@@ -26,4 +32,5 @@ $results|ForEach-Object {
         $gathered += $herbs | Where-Object Biome -like $biome | Where-Object Rarity -like "legendary" | Get-Random
     }    
 }
+#displays results
 $gathered|Sort-Object -Property Name|group-object -Property Name|Select-Object name,count
