57afc704ead6b13e2740416f07a5ae8415a00044
Kevin Burkeland <kevin@burke.land>
2022-12-05 13:32:38 -0800

    added advantage & disadvantage

--- animateobj.ps1
+++ animateobj.ps1
@@ -12,26 +12,38 @@ Write-Host "[1] Tiny Object
 [5] Huge Object"
 [int]$objectSize = Read-Host -Prompt "Enter the size of the objects you are animating"
 [int]$targetAC = Read-Host -Prompt "Enter the target AC"
+$Adv = Read-Host -Prompt "Do you have advantage? [y/n]"
+$Adv = switch ($Adv.ToLower) {
+    {$_ -in "y","yes"} { $true }
+    {$_ -in "n","no"} { $false }
+    Default {$false}
+}
+$Dis = Read-Host -Prompt "Do you have disadvantage? [y/n]"
+$Dis = switch ($Dis.ToLower) {
+    {$_ -in "y","yes"} { $true }
+    {$_ -in "n","no"} { $false }
+    Default {$false}
+}
 #rolls the requested number of d20s
 switch ($objectSize) {
     1 {
-        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 8
+        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 8 -Advantage $Adv -Disadvantage $Dis
         $damage=Get-Damage -Sides 4 -NumDice 1 -Bonus 4 -Hits $hits[0] -Crit $hits[1]
     }
     2 {
-        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 6
+        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 6 -Advantage $Adv -Disadvantage $Dis
         $damage=Get-Damage -Sides 8 -NumDice 1 -Bonus 2 -Hits $hits[0] -Crit $hits[1]
     }
     3 {
-        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 5
+        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 5 -Advantage $Adv -Disadvantage $Dis
         $damage=Get-Damage -Sides 6 -NumDice 2 -Bonus 1 -Hits $hits[0] -Crit $hits[1]
     }
     4 {
-        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 6
+        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 6 -Advantage $Adv -Disadvantage $Dis
         $damage=Get-Damage -Sides 10 -NumDice 2 -Bonus 2 -Hits $hits[0] -Crit $hits[1]
     }
     5 {
-        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 8
+        $hits=Get-ToHit -Attacks $objectNum -AC $targetAC -Bonus 8 -Advantage $Adv -Disadvantage $Dis
         $damage=Get-Damage -Sides 12 -NumDice 2 -Bonus 4 -Hits $hits[0] -Crit $hits[1]
     }
     Default { Write-Host "Invalid size, select a number 1-5" }
--- modules/tohit.ps1
+++ modules/tohit.ps1
@@ -1,12 +1,35 @@
-
+using module ./dice.psm1
+$dice = [Dice]::new()
 function Get-ToHit {
     [CmdletBinding()]
     param (
         [int]$Attacks,
         [int]$AC,
-        [int]$Bonus
+        [int]$Bonus,
+        [bool]$Advantage = $false,
+        [bool]$Disadvantage = $false
     )
-    $attackRoll = $dice.GetD20($Attacks)
+    if (($Advantage) -xor ($Disadvantage)) {
+        $attackRoll = $dice.GetD20($Attacks)
+    }
+    elseif ($Advantage) {
+        $attackRoll = $dice.GetD20($Attacks)
+        $attackRoll2 = $dice.GetD20($Attacks)
+        for ($i = 0; $i -lt $attackRoll.Count; $i++) {
+            if ($attackRoll2[$i] -gt $attackRoll[$i]) {
+                $attackRoll[$i] = $attackRoll2[$i]
+            }
+        }
+    }
+    else {
+        $attackRoll = $dice.GetD20($Attacks)
+        $attackRoll2 = $dice.GetD20($Attacks)
+        for ($i = 0; $i -lt $attackRoll.Count; $i++) {
+            if ($attackRoll2[$i] -lt $attackRoll[$i]) {
+                $attackRoll[$i] = $attackRoll2[$i]
+            }
+        }
+    }
     $numHit = 0
     $bonusDice = 0
     for ($i = 0; $i -lt $attackRoll.Count; $i++) {
