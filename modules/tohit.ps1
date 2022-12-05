d241295e371a17a5d5d8e6162d97fe65cf33eb45
Kevin Burkeland <kevin@burke.land>
2022-12-05 10:15:36 -0800

    started work on tohit function

---
+++ modules/tohit.ps1
@@ -0,0 +1,27 @@
+using module ./dice.psm1
+$dice = [Dice]::new()
+function get-tohit {
+    [CmdletBinding()]
+    param (
+        [int]$Attacks,
+        [int]$AC,
+        [int]$Bonus
+    )
+    $attackRoll = $dice.GetD20($Attacks)
+    $numHit = 0
+    $bonusDice = 0
+    for ($i = 0; $i -lt $attackRoll.Count; $i++) {
+        if (($attackRoll[$i]+$Bonus) -ge $AC) {
+            $numHit++
+            #bonus dice for crits
+            if ($attackRoll[$i] -eq 20) {
+                $bonusDice++
+            }
+        }
+        #auto hit on 20 if ac too high
+        elseif ($attackRoll[$i] -eq 20) {
+            $numHit++
+        }
+    }
+    $numHit, $bonusDice
+}
