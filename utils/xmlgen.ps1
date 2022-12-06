c6fcfcc389f8a529dc686c8d05603bf731efff88
Kevin Burkeland <kevin@burke.land>
2022-12-06 10:08:04 -0800

    Update xmlgen.ps1

--- utils/xmlgen.ps1
+++ utils/xmlgen.ps1
@@ -1,9 +1,12 @@
-
+#imports herb data (run once at beginning of adding new biomes)
+#$herbs = Import-Clixml -Path .\Data\herbs.xml
 #build the object
-Get-Content -Path C:\Users\kburkelands\Documents\Github\Powershell-dnd\utils\ondeck.txt|ForEach-Object {
+Get-Content -Path .\ondeck.txt|ForEach-Object {
     $herbs += New-Object psobject -Property @{
         Name = $_
         Rarity = "Legendary"
         Biome = "Caves"
     }
 }
+#export the object (run once at end of adding new biomes)
+#$herbs|Export-Clixml -Path .\Data\herbs.xml
